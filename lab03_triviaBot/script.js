b464076b14c4c05e119e3dd0cc6f5367de6c6175
Dale Lee <daleandrewslee@gmail.com>
2025-12-21 23:37:08 +0800

    concatenation

---
+++ lab03_triviaBot/script.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "John";
+let botLocation = "Philippines";
+let favoriteLanguage = "JavaScript";
+let codingFact = "Today i'm learning about the language" + favoriteLanguage + "."
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+console.log(codingFact)
+
+codingFact = "I enjoy practicing " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is fun because it can run in the browser!";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
